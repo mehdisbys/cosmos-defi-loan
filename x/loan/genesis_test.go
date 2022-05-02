5440b60c16b1bd67f36b1cf4ddc649dc4017cd07
Mehdi <mehdi.souihed@gmail.com>
2022-05-02 12:47:31 +0100

    Scaffold loan module and loan list

---
+++ x/loan/genesis_test.go
@@ -0,0 +1,40 @@
+package loan_test
+
+import (
+	"testing"
+
+	keepertest "github.com/mehdisbys/loan/testutil/keeper"
+	"github.com/mehdisbys/loan/testutil/nullify"
+	"github.com/mehdisbys/loan/x/loan"
+	"github.com/mehdisbys/loan/x/loan/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		LoanList: []types.Loan{
+			{
+				Id: 0,
+			},
+			{
+				Id: 1,
+			},
+		},
+		LoanCount: 2,
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.LoanKeeper(t)
+	loan.InitGenesis(ctx, *k, genesisState)
+	got := loan.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	require.ElementsMatch(t, genesisState.LoanList, got.LoanList)
+	require.Equal(t, genesisState.LoanCount, got.LoanCount)
+	// this line is used by starport scaffolding # genesis/test/assert
+}
